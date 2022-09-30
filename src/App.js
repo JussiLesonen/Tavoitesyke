4e07d0e36d9ec0b177ed7907801ebc3632fc67a1
Jussi <t1leju02@students.oamk.fi>
2022-09-30 10:42:52 +0300

    Valmis nettisivu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [age, setAge] = useState(0)
+  const [lower, setLower] = useState(0)
+  const [upper, setUpper] = useState(0)
+
+  function calculate(e) {
+    e.preventDefault()
+    setLower((220 - age) * 0.65)
+    setUpper((220 - age) * 0.85)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="content">
+      <hr />
+      <h3>Heart rate limits calculator</h3>
+      <form action="" onSubmit={calculate}>
+        <div>
+          <label htmlFor="">Age</label>
+          <input type="text" value={age} onChange={e => setAge(e.target.value)}/>
+        </div>
+        <div>
+          <label htmlFor="">Heart rate limits</label>
+          <output>{lower.toFixed(2)} - {upper.toFixed(2)}</output>
+          <button>Calculate</button>
+        </div>
+      </form>
     </div>
   );
 }
